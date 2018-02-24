ce9dc316149c2495e77e4f989795e1367b3ecff6
lithium2013 <379129295@qq.com>
2018-02-24 17:20:09 +0800

    [UPDATE]add config && ]readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # entry_task_server
+
+## Install 
+  `npm install`
+
+## Config
+  See in `config.yml`, port 3000 will be listened as default.
+
+## Usage
+  - `npm start` to start server.
+  - DB file `db/databasesqlite` will be `created/reset` while first running
--- src/app.js
+++ src/app.js
@@ -1,16 +1,22 @@
 'use strict'
+const fs = require('fs')
 
+const yaml = require('js-yaml')
 const fastify = require('fastify')()
 const cookieParser = require('cookie-parser')
+
 const models = require('./models/index')
 const router = require('./router/index')
+
+const config = yaml.safeLoad(fs.readFileSync('./config.yml'))
+console.log(config)
 
 fastify.register(models)
 
 fastify.use('/', cookieParser())
 fastify.register(router, { prefix: '/api/v1' })
 
-fastify.listen(3000, err => {
+fastify.listen(config.port, err => {
   if (err) {
     console.log(err)
     fastify.log.error(err)
